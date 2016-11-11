1658ec001e4f2f014a978b75da397ece8ffa7740
Sucipto <show.cheap@gmail.com>
2016-11-12 06:23:07 +0700

    Fungsi menampilkan harga

--- app/src/main/java/com/pringstudio/androidkejar2/MainActivity.java
+++ app/src/main/java/com/pringstudio/androidkejar2/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
 
     // Snippet bisa dicopy di sini: https://git.io/vXKTW
@@ -20,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
         // Panggil Fungsi saat tombol di klik
         displayOrder(2);
 
-        displayOrder(7 * 2);
+        // Panggil fungsi untuk menampilkan harga
+        displayPrice(2 * 5);
     }
 
     // Aksi untuk menampilkan order
@@ -29,5 +32,10 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityOrderText = (TextView) findViewById(R.id.text_quantity);
         // Ubah Text
         quantityOrderText.setText(""+number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceText = (TextView) findViewById(R.id.text_price);
+        priceText.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
